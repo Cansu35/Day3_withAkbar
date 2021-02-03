ab8d831d24fa2bf77dd15f457f5f2f252dbceda7
Cansu35 <71334287+Cansu35@users.noreply.github.com>
2021-02-04 00:45:59 +0100

    Class 21 is dedicated

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola chicos");
         //Group 21 is excited
+        //Class 21 is dedicated
     }
 }
